646c048b841bc1d37861865bbe5b5cbc8baa34ce
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-08-26 15:35:12 -0300

    Importando imagens

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
 import './App.css';
 
+import profile from './assets/kakashi.jpg';
+
 function App() {
-    return <h1>Hello Rocketseat</h1>
+    return <img src={profile} width="200" />
 }
 
 export default App;
